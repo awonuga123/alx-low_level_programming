03e32d77a0fdfca4d84f81838b7fa6228cd5b00e
awonuga ismail <awonugaismail132@gmail.com>
2022-06-03 10:59:08 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch = 0;
+
+	while (ch < 10)
+	{
+		printf("%d", ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
