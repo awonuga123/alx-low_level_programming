969ee5315bd9a4ad87a7ee18db6cb1e577a8e925
awonuga ismail <awonugaismail132@gmail.com>
2022-06-03 11:05:09 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 0;
+
+	while (ch < 10)
+	{
+		putchar(ch + '0');
+		ch++;
+	}
+	putchar('\n');
+	return (0);
