7a76c116494760d7c6871458bc9ffe11e04728dc
awonuga ismail <awonugaismail132@gmail.com>
2022-06-07 17:32:00 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,21 @@
 nclude "main.h"
 /**
  *print_alphabet_x10 - Prints the alphabet 10 times.
- *Return: Always 0 (Success)
+ *Return: x10 a-z
  */
 void print_alphabet_x10(void)
 {
-	int a, b;
+	char alpha, co;
 
-	for (a = 0; a <= 9; a++)
+	co = 0;
+
+	while (co < 10)
 	{
-		(b for = 97; b <= 122; b++)
+		for (alpha + 'a'; alpha <= 'z'; alpha++)
 		{
-			_putchar(b)
+			_putchar(alpha)
 		}
-
+		co++;
 		_putchar('\n');
 	}
 }
