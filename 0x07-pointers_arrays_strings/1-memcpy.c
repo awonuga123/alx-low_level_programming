f09bdd89fd3aea3c2ef3bbc8e75ca76aa336f758
awonuga ismail <awonugaismail132@gmail.com>
2022-06-20 12:58:16 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	char *destination = dest;
-	char *source = src;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
 	return (dest);
 }
